c80171e1603d3fc4c66f38b0344a3e88acd8b706
Mehedy Hasan Piale <mehedyhasanpialjvai@gmail.com>
2026-03-18 06:06:47 +0600

    feat: some work has been updated

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -18,3 +18,38 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     return payload;
   }
 }
+
+// import { Injectable } from '@nestjs/common';
+// import { ExtractJwt, Strategy } from 'passport-jwt';
+// import { PassportStrategy } from '@nestjs/passport';
+
+// // ✅ Extract from cookie
+// const cookieExtractor = (req: any): string | null => {
+//   if (req?.cookies?.accessToken) {
+//     return req.cookies.accessToken;
+//   }
+//   return null;
+// };
+
+// @Injectable()
+// export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+//   constructor() {
+//     const secret = process.env.JWT_SECRET;
+
+//     if (!secret) {
+//       throw new Error('JWT_SECRET is not defined');
+//     }
+
+//     super({
+//       jwtFromRequest: ExtractJwt.fromExtractors([
+//         cookieExtractor, // ✅ 1st priority (browser)
+//         ExtractJwt.fromAuthHeaderAsBearerToken(), // ✅ fallback (Postman)
+//       ]),
+//       secretOrKey: secret,
+//     });
+//   }
+
+//   async validate(payload: any) {
+//     return payload;
+//   }
+// }
